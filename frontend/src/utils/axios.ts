ee08036f6616ce103c30d5536ad6738d7bfbe5cf
Chamindu-Dharmawickrema <chamindudharmawickrema@gmail.com>
2026-01-21 13:32:50 +0530

    Update axios.ts

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
     timeout: 120000,
 });
+
+console.log("Api base url ", API_BASE_URL)
 
 // Request interceptor
 api.interceptors.request.use(
